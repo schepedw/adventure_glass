92933f1f07405f51ff2a5118148da4cc80ef61c8
Daniel Schepers <schepedw@gmail.com>
2015-02-26 21:18:31 -0600

    add galleries controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#main'
-    get 'boats/build' => 'boats#build'
-    get 'boats/index' => 'boats#index'
+  get 'boats/build' => 'boats#build'
+  get 'boats/index' => 'boats#index'
+  get 'gallery' => 'galleries#all'
+  get 'gallery/boats' => 'galleries#boats'
 end
