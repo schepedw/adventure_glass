badc45c1add06dc8740f0077e56e07816a1e8b2a
Daniel Schepers <dschepers@enova.com>
2015-09-10 21:28:46 -0500

    knowing how to program would be neat

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,6 @@ def base_model_range
   BaseModel.all.map(&:id)
 end
 
-def user
-  @a ||= User.find_by_email('test@test.com')
-  return @a if @a.present?
-  @a = User.create(email: 'test@test.com', password: 'test', shipping_addresses: [ShippingAddress.first])
-  @a.add_role :admin
-  @a.save!
-end
-
 def option_range
   (Option.first.id .. Option.last.id).to_a
 end
@@ -102,12 +94,5 @@ end
   Lift.create(price: Faker::Commerce.price.to_i, name: Faker::Commerce.product_name, available_options: [Option.find(option_range.sample)], specifications: [Specification.find(spec_range.sample)], image_path: 'lifts', description: Faker::Lorem.sentence)
 end
 
-ShippingAddress.create(building_number: '510', street: 'N Wesley Dr.', city: 'Jasper',
-                       state: 'IN', zip_code: '47546')
 parts
 boats
-cart = ShoppingCart.create(user: user)
-50.times {|i| Product.create(shopping_cart: cart, base_model_id: base_model_range.sample, quantity: rand(10))}
-cart.product_ids = Product.all.select{|p| p.id % 3 == 0 }.map(&:id)
-user.update_attributes(shopping_cart: cart)
-Order.create(shopping_cart: cart, user: user)
