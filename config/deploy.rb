2067cf1e35196714ce35b64cced113bf103d9720
Daniel Schepers <dschepers@enova.com>
2015-10-25 20:32:28 -0500

    compress images using image optim

--- config/deploy.rb
+++ config/deploy.rb
@@ -92,6 +92,7 @@ after 'deploy:assets:precompile', 'uploads:create_symlinks'
 namespace :uploads do
   task :create_symlinks do
     run "cd #{shared_path} && mkdir -p base_model_pictures"
-    run "cd #{release_path}/public/assets && if ! [ -L ./base_model_pictures ]; then ln -s #{shared_path}/base_model_pictures base_model_pictures; fi"
+    run "cd #{release_path}/public && mkdir -p images"
+    run "cd #{release_path}/public/images && if ! [ -L ./base_model_pictures ]; then ln -s #{shared_path}/base_model_pictures base_model_pictures; fi"
   end
 end
