1be8ab1e0e8faefac911cf3c9497de4e8519a996
Daniel Schepers <dschepers@enova.com>
2015-12-22 16:34:17 -0600

    rm unneeded files

--- db/migrate/20151217031636_seed_statuses.rb
+++
@@ -1,7 +0,0 @@
-class SeedStatuses < ActiveRecord::Migration
-  def change
-    %w[new confirmed processing processed shipping complete].each do |status|
-      Status.create(status: status)
-    end
-  end
-end
